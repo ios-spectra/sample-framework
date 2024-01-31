916d3bfa01bc6e96e28ef8b01e09ef5743ea61c5
ios-spectra <158260985+ios-spectra@users.noreply.github.com>
2024-01-31 18:41:19 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,11 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "NewSampleFW",
                       url: "https://github.com/ios-spectra/sample-framework/releases/download/1.0/NewSampleFW.xcframework.zip",
-                      checksum: "1792ac386452ec82291adadee1c4509b9743bbb1160a562cf9377813edf42f67"),
-        .target(
-            name: "NewSampleFW"),
-        .testTarget(
-            name: "NewFWTests",
-            dependencies: ["NewSampleFW"]),
+                      checksum: "1792ac386452ec82291adadee1c4509b9743bbb1160a562cf9377813edf42f67")
     ]
 )
