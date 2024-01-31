eb433ee17a4b69401b9655a43df894a732ab92cd
ios-spectra <158260985+ios-spectra@users.noreply.github.com>
2024-01-31 18:26:35 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "NewFW",
+    name: "NewSampleFW",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "NewFW",
-            targets: ["NewFW"]),
+            name: "NewSampleFW",
+            targets: ["NewSampleFW"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -18,9 +18,9 @@ let package = Package(
                       url: "https://github.com/ios-spectra/sample-framework/releases/download/1.0/NewSampleFW.xcframework.zip",
                       checksum: "1792ac386452ec82291adadee1c4509b9743bbb1160a562cf9377813edf42f67"),
         .target(
-            name: "NewFW"),
+            name: "NewSampleFW"),
         .testTarget(
             name: "NewFWTests",
-            dependencies: ["NewFW"]),
+            dependencies: ["NewSampleFW"]),
     ]
 )
